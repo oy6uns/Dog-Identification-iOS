4b05251cf714e896bc1fe7557e64348ac3f55489
Saint Bae <45239582+oy6uns@users.noreply.github.com>
2023-04-29 03:05:12 +0900

    [Feat] POST 통신 구현
    multipartformData로 사진을 보내면, 개의 품종을 알려주는 POST 통신 구현

---
+++ Network/ImageResponseDto.swift
@@ -0,0 +1,14 @@
+//
+//  ImageResponseDto.swift
+//  PicPractice
+//
+//  Created by saint on 2023/04/28.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct ImageResponseDto: Codable {
+    let success: Bool
+    let message, breed: String
+}
